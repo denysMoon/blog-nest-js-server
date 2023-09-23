4bdadeb3822f41b2d0df0ca9e9127b64f61fa820
Denis Ulvanskyi <denis.ulvanskyi@cengage.com>
2023-09-24 00:45:59 +0300

    login flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from 'src/user/dto/new-user';
 import { UserDetails } from 'src/user/user-details.interface';
+import { ExistingUserDto } from 'src/user/dto/existing-user';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,11 @@ export class AuthController {
     @Body() user: RegisterUserDto,
   ): Promise<UserDetails | null | string> {
     return this.authService.register(user);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() user: ExistingUserDto): Promise<{ token: string } | string> {
+    return this.authService.login(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { ExistingUserDto } from 'src/user/dto/existing-user';
 import { RegisterUserDto } from 'src/user/dto/new-user';
 import { UserDetails } from 'src/user/user-details.interface';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, 10);
@@ -28,5 +33,50 @@ export class AuthService {
     const newUser = await this.userService.create(name, email, hashPassword);
 
     return this.userService._getUserDetails(newUser);
+  }
+
+  async isPassportMath(
+    password: string,
+    hashPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | string> {
+    const user = await this.userService.findOneByEmail(email);
+
+    if (!user) {
+      return 'This user does not exist';
+    }
+
+    const isPasswordMatch = await this.isPassportMath(password, user.password);
+
+    if (!isPasswordMatch) {
+      return 'Password is incorrect';
+    }
+
+    return this.userService._getUserDetails(user);
+  }
+
+  async login(
+    existingUser: ExistingUserDto,
+  ): Promise<{ token: string } | null> {
+    const { email, password } = existingUser;
+
+    const user = await this.validateUser(email, password);
+
+    if (
+      user === 'This user does not exist' ||
+      user === 'Password is incorrect'
+    ) {
+      return null;
+    }
+
+    const jwt = await this.jwtService.signAsync({ user });
+
+    return { token: jwt };
   }
 }
